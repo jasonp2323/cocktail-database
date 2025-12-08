b2ff18e7a2ccb1480503d02d5ccbd2876c35a51d
jasonp-bitg <jasonp@beginneritguides.com>
2025-12-08 16:58:13 -0500

    Updated init.sh

--- SQL/init.sh
+++ SQL/init.sh
@@ -2,13 +2,13 @@
 set -e
 
 echo "Waiting for Oracle to be ready..."
-until echo "SELECT 1 FROM DUAL;" | sqlplus -s system/${ORACLE_PWD}@XE > /dev/null 2>&1; do
+until echo "SELECT 1 FROM DUAL;" | sqlplus -s SYS/${ORACLE_PWD}@XE > /dev/null 2>&1; do
   sleep 5
 done
 
 echo "Creating database users..."
 
-sqlplus -s system/${ORACLE_PWD}@XE <<EOF
+sqlplus -s SYS/${ORACLE_PWD}@XE <<EOF
 -- Create users
 CREATE USER java_user IDENTIFIED BY ${LOGIN_DB_PASSWORD};
 CREATE USER csc_cocktails IDENTIFIED BY ${APP_DB_PASSWORD};
