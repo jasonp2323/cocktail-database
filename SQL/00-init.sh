31dec6a912741547380151d930545a9fc7687664
jasonp-bitg <jasonp@beginneritguides.com>
2025-12-08 18:42:01 -0500

    TEST8

---
+++ SQL/00-init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "Creating database users..."
+sqlplus system/${ORACLE_PWD}@XE <<EOF
+CREATE USER java_user IDENTIFIED BY ${LOGIN_DB_PASSWORD};
+CREATE USER csc_cocktails IDENTIFIED BY ${APP_DB_PASSWORD};
+GRANT CONNECT, RESOURCE, CREATE SESSION TO java_user;
+GRANT CONNECT, RESOURCE, CREATE SESSION TO csc_cocktails;
+GRANT UNLIMITED TABLESPACE TO java_user;
+GRANT CREATE TABLE TO java_user;
+GRANT CREATE TABLE TO csc_cocktails;
+EXIT;
+EOF
+
+echo "Database initialization complete!"
