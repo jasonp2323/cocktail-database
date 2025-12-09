5eea7d9a6df44fb201084c3c69857e372a97847c
jasonp-bitg <jasonp@beginneritguides.com>
2025-12-08 19:04:08 -0500

    TEST10

--- SQL/00-init.sh
+++ SQL/00-init.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Creating database users..."
-sqlplus sqlplus system/${ORACLE_PWD}@XEPDB1 <<EOF
+sqlplus system/${ORACLE_PWD}@XEPDB1 <<EOF
 CREATE USER java_user IDENTIFIED BY ${LOGIN_DB_PASSWORD};
 CREATE USER csc_cocktails IDENTIFIED BY ${APP_DB_PASSWORD};
 GRANT CONNECT, RESOURCE, CREATE SESSION TO java_user;
